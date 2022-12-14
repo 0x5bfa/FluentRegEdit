4e5497c58f70f5c991d99f2fcd0b7a23aff1ca6c
U+5BFA <onein528@outlook.com>
2022-12-14 22:56:43 +0900

    Remove RegistryValley.Console

--- src/RegistryValley.App/ViewModels/SettingsViewModel.cs
+++ src/RegistryValley.App/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,34 @@
-﻿namespace RegistryValley.App.ViewModels
+﻿using Microsoft.UI.Xaml;
+using RegistryValley.App.Services;
+
+namespace RegistryValley.App.ViewModels
 {
-    public class SettingsViewModel
+    public class SettingsViewModel : ObservableObject
     {
+        public SettingsViewModel()
+        {
+            ColorModes = new List<string>()
+            {
+                "Default",
+                "Light",
+                "Dark",
+            }
+            .AsReadOnly();
+        }
+
+        public ReadOnlyCollection<string> ColorModes { get; set; }
+
+        private int _selectedColorModeIndex;
+        public int SelectedColorModeIndex
+        {
+            get => _selectedColorModeIndex;
+            set
+            {
+                if (SetProperty(ref _selectedColorModeIndex, value))
+                {
+                    ThemeModeServices.RootTheme = (ElementTheme)value;
+                }
+            }
+        }
     }
 }
--- src/RegistryValley.App/Views/SettingsPage.xaml.cs
+++ src/RegistryValley.App/Views/SettingsPage.xaml.cs
@@ -1,31 +1,22 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using RegistryValley.App.Dialogs;
+using RegistryValley.App.Models;
+using RegistryValley.App.ViewModels;
 
 namespace RegistryValley.App.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class SettingsPage : Page
     {
         public SettingsPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            var provider = App.Current.Services;
+            ViewModel = provider.GetRequiredService<SettingsViewModel>();
         }
+
+        public SettingsViewModel ViewModel { get; }
     }
 }
